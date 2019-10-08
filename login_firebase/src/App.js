ba516e4c1c6f565c8991cebba0711c099635d087
Noel Blandon <noel.f.blandon@gmail.com>
2019-10-08 17:48:59 -0600

    Passing data between sibling components in React using Context API and React Hooks

--- login_firebase/src/App.js
+++ login_firebase/src/App.js
@@ -1,41 +1,109 @@
-import React from 'react';
+import React, { useReducer, useContext } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+//Material-UI Components
+import { Container, Typography, Grid, CssBaseline, Toolbar } from  '@material-ui/core';
+
+//import own components
+
+import Input_one from './components/Input_one';
+import Input_two from './components/Input_two';
+
+ // Firebase Config
+ const firebaseConfig = {
+  apiKey: "AIzaSyAqlQDZdMEcvk5Cw4-_f24gEQvuVphJO5I",
+  authDomain: "reactjs-login-451ec.firebaseapp.com",
+  databaseURL: "https://reactjs-login-451ec.firebaseio.com",
+  projectId: "reactjs-login-451ec",
+  storageBucket: "",
+  messagingSenderId: "574724937852",
+  appId: "1:574724937852:web:d15381a64ddc5f861e51f9",
+  measurementId: "G-M9XXD4XZ1R"
+};
+
+
+const styles = makeStyles({
+    toolbarTitle: {
+      flex:1,
+    },
+});
+
+// Create context object
+export const AppContext = React.createContext();
+
+// Set up Initial State
+const initialState = {
+
+    inputText: '',
+
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'UPDATE_INPUT':
+            return {
+                inputText: action.data
+            };
+
+
+        default:
+            return initialState;
+    }
+}
 
 function App() {
 
-  // Firebase Config
-  const firebaseConfig = {
-    apiKey: "AIzaSyAqlQDZdMEcvk5Cw4-_f24gEQvuVphJO5I",
-    authDomain: "reactjs-login-451ec.firebaseapp.com",
-    databaseURL: "https://reactjs-login-451ec.firebaseio.com",
-    projectId: "reactjs-login-451ec",
-    storageBucket: "",
-    messagingSenderId: "574724937852",
-    appId: "1:574724937852:web:d15381a64ddc5f861e51f9",
-    measurementId: "G-M9XXD4XZ1R"
-  };
-
-
-
+  const classes = styles();
+ 
+  const [state, dispatch] = useReducer(reducer, initialState);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-     
+    <div>
+      <Container maxWidth="lg">
+        <CssBaseline />
+        {/* Title */}
+        <Toolbar>
+          <Typography
+            component="h2"
+            variant="h5"
+            color="inherit"
+            align="center"
+            noWrap
+            className={classes.toolbarTitle}>
+            Pass Data Between react sibling components
+          </Typography>
+        </Toolbar>
+
+        {/*Inputs*/}
+        <Grid container spacing={1}>
+          <AppContext.Provider value={{ state, dispatch }}>
+            <Input_one/>
+            <Input_two/>
+          </AppContext.Provider>  
+        </Grid>
+      </Container>
+
+      <!-- div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      
+      </div -->
+
     </div>
   );
 }
---
+++ login_firebase/src/components/Input_one.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles/index';
+
+//Material UI Components
+import {Grid, Paper, InputBase, IconButton } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles ({
+    root: {
+        padding:'2px 4px',
+        display:'flex',
+        alignItems:'center',
+        width:'100%',
+    },
+    input: {
+        marginLeft:'8px',
+            flex:1,
+
+    },
+    IconButton: {
+        padding:10,
+    }
+});
+
+export default function Input_one () {
+
+    const classes = useStyles();
+
+    return (
+     <React.Fragment>
+         <Grid xs={12} md={6}>
+            <Paper className={classes.root}>
+                <InputBase
+                    className={classes.input}
+                    placeholder="Input One"
+                />
+                <IconButton
+                    className={classes.iconButton}
+                    arial-label="search"
+                >
+                    <SearchIcon />
+                </IconButton>
+            </Paper>
+         </Grid>
+    </React.Fragment>            
+    );
+
+
+};
+
